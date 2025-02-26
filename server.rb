c67d3a585c9ae9af4d4c660b2e799fae0f7efa03
Kyle Roberts <kyle@philliptown.dev>
2025-02-26 12:55:55 -0500

    Added HTMX Routes & Sessions

--- server.rb
+++ server.rb
@@ -15,7 +15,33 @@ set :views, File.join(settings.root, 'src', 'views')
 set :port, 4567  # Default Sinatra port
 set :bind, '0.0.0.0'  # Allows access from other devices on your network
 
+# Enable Sessions
+use Rack::Session::Cookie, key: 'rack.session',
+                           path: '/',
+                           expire_after: 2592000, # 30 days
+                           secret: 'a_very_long_and_secure_random_secret_key_that_is_at_least_64_bytes_long'
+
+
+
 get '/' do
+  # puts "Session ID: #{session.inspect}"
   @title = 'Sinatra TAH'
   erb :index, layout: :'layouts/main'
+end
+
+# HTMX Demo Route
+get '/demo' do
+  session[:count] ||= 0
+  session[:count] += 1
+  @count = session[:count]
+  puts "The current count @ #{Time.now} is: #{@count}"
+
+  erb :'partials/demo', layout: false
+end
+
+# Session Clear Route
+# This route is used to clear the session count
+get '/clear' do
+  session.clear
+  redirect '/'
 end
